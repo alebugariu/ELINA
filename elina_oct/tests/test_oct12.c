0e6947255af21199ad24b5ef09f76c409db16c96
Alexandra Bugariu <alexandra.bugariu@inf.ethz.ch>
2017-05-05 13:15:20 +0000

    added the tests for all the lattice properties (except widening)

---
+++ elina_oct/tests/test_oct12.c
@@ -0,0 +1,41 @@
+#include <time.h>
+#include <klee/klee.h>
+#include "opt_oct.h"
+#include "opt_oct_internal.h"
+#include "opt_oct_hmat.h"
+#include "test_oct.h"
+#include <string.h>
+#include <stdio.h>
+
+
+bool is_join_compatible_direct(elina_manager_t * man, opt_oct_t * x, opt_oct_t * y) {
+	if (opt_oct_is_leq(man, x, y)) {
+		return opt_oct_is_eq(man, opt_oct_join(man, false, x, y), y);
+	}
+	return true;
+}
+
+int main(int argc, char **argv) {
+	unsigned short int dim;
+	size_t nbcons;
+	klee_make_symbolic(&dim, sizeof(dim), "number of variables");
+	klee_make_symbolic(&nbcons, sizeof(nbcons), "number of constraints");
+	klee_assume(dim > 0);
+	klee_assume(nbcons > 0);
+
+	elina_manager_t * man = opt_oct_manager_alloc();
+	opt_oct_t * top = opt_oct_top(man, dim, 0);
+	opt_oct_t * bottom = opt_oct_bottom(man, dim, 0);
+
+	opt_oct_t* octagon1 = create_octagon(man, top, "1", dim, nbcons);
+	opt_oct_t* octagon2 = create_octagon(man, top, "2", dim, nbcons);
+
+	//meet == glb, join == lub
+	//join is compatible (direct)
+	klee_assert(is_join_compatible_direct(man, octagon1, octagon2));
+	return 0;
+}
+
+
+
+
---
+++ elina_oct/tests/test_oct24.c
@@ -0,0 +1,33 @@
+#include <time.h>
+#include <klee/klee.h>
+#include "opt_oct.h"
+#include "opt_oct_internal.h"
+#include "opt_oct_hmat.h"
+#include "test_oct.h"
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	unsigned short int dim;
+	size_t nbcons;
+	klee_make_symbolic(&dim, sizeof(dim), "number of variables");
+	klee_make_symbolic(&nbcons, sizeof(nbcons), "number of constraints");
+	klee_assume(dim > 0);
+	klee_assume(nbcons > 0);
+
+	elina_manager_t * man = opt_oct_manager_alloc();
+	opt_oct_t * top = opt_oct_top(man, dim, 0);
+	opt_oct_t * bottom = opt_oct_bottom(man, dim, 0);
+
+	opt_oct_t* octagon1 = create_octagon(man, top, "1", dim, nbcons);
+	opt_oct_t* octagon2 = create_octagon(man, top, "2", dim, nbcons);
+
+	//meet == glb, join == lub
+	//meet absorbtion
+	klee_assert(opt_oct_is_eq(man, opt_oct_meet(man, false, octagon1, opt_oct_join(man, false, octagon1, octagon2)), octagon1));
+	return 0;
+}
+
+
+
+
