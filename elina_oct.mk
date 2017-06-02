9a054bf4dc36dae8b96eee63e78acf6558f3157c
Alexandra Bugariu <alexandra.bugariu@inf.ethz.ch>
2017-06-02 09:14:11 +0000

    renamed the makefile script and added instruction on how to compile elina with clang

--- elina_oct.mk
+++ elina_oct.mk
@@ -1,8 +1,18 @@
 number ?= 24
 start ?= 0
-all:
+
+all: compile test
+compile: 
+	clang -I /home/klee/klee_src/include -emit-llvm -c -g elina_auxiliary/*.c; \
+	clang -I /home/klee/klee_src/include -emit-llvm -c -g elina_linearize/*.c; \
+	clang -I /home/klee/klee_src/include -emit-llvm -c -g partitions_api/*.c; \
+	clang -I /home/klee/klee_src/include -emit-llvm -c -g elina_oct/*.c; \
+
+ 
+
+test:
 	cd elina_oct/tests; \
-        rm -r klee-*; \
+        rm -rf klee-*; \
 	clang -I /home/klee/klee_src/include -emit-llvm -c -g *.c ; \
         number=$(start) ; while [ $${number} -le $(number) ] ; do \
 		llvm-link ../../elina_auxiliary/*.bc ../../partitions_api/*.bc ../../elina_linearize/*.bc ../opt_oct_closure_comp_sparse.bc ../opt_oct_incr_closure_comp_sparse.bc ../opt_oct_closure_dense.bc ../opt_oct_incr_closure_dense.bc ../opt_oct_nary.bc ../opt_oct_resize.bc ../opt_oct_predicate.bc ../opt_oct_representation.bc ../opt_oct_transfer.bc ../opt_oct_hmat.bc ../opt_oct_closure_dense_scalar.bc ../opt_oct_incr_closure_dense_scalar.bc test_oct.bc test_oct$${number}.bc -o test$${number}.bc ; \
