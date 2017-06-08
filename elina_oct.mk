4cd921a4b01e97991dfce490ec7859a544752df1
Alexandra Bugariu <alexandra.bugariu@inf.ethz.ch>
2017-06-08 16:42:56 +0000

    added the tests for narrowing

--- elina_oct.mk
+++ elina_oct.mk
@@ -1,4 +1,4 @@
-number ?= 24
+number ?= 39
 start ?= 0
 
 all: compile test
@@ -20,7 +20,7 @@ test:
         number=$(start) ; while [ $${number} -le $(number) ] ; do \
 		llvm-link ../../elina_auxiliary/*.bc ../../partitions_api/*.bc ../../elina_linearize/*.bc ../opt_oct_closure_comp_sparse.bc ../opt_oct_incr_closure_comp_sparse.bc ../opt_oct_closure_dense.bc ../opt_oct_incr_closure_dense.bc ../opt_oct_nary.bc ../opt_oct_resize.bc ../opt_oct_predicate.bc ../opt_oct_representation.bc ../opt_oct_transfer.bc ../opt_oct_hmat.bc ../opt_oct_closure_dense_scalar.bc ../opt_oct_incr_closure_dense_scalar.bc test_oct.bc test_oct$${number}.bc -o test$${number}.bc ; \
                 startTime=`date +%s` ; \
-                klee -allow-external-sym-calls -sym-malloc-bound=128 -max-depth=6 test$${number}.bc ; \
+                klee -allow-external-sym-calls -sym-malloc-bound=128 -max-depth=10 test$${number}.bc ; \
                 endTime=`date +%s` ; \
                 runtime=`expr $$endTime - $$startTime` ; \
                 echo "Execution time: $$runtime sec for test_oct$$number\n" ; \
---
+++ elina_oct/tests/test_oct36.c
@@ -0,0 +1,26 @@
+#include <time.h>
+#include <klee/klee.h>
+#include "opt_oct.h"
+#include "opt_oct_internal.h"
+#include "opt_oct_hmat.h"
+#include "test_oct.h"
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	unsigned short int dim;
+	make_symbolic_dimension(&dim);
+
+	elina_manager_t * man = opt_oct_manager_alloc();
+	opt_oct_t * top = opt_oct_top(man, dim, 0);
+	opt_oct_t * bottom = opt_oct_bottom(man, dim, 0);
+
+	opt_oct_t* octagon1 = create_octagon(man, top, "1", dim);
+
+	//meet == glb, join == lub
+	//bottom narrowing x == bottom
+	klee_assert(
+			opt_oct_is_bottom(man, opt_oct_narrowing(man, bottom, octagon1)));
+	return 0;
+}
+
