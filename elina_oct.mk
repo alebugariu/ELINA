d3d2e26726ab11ab748870094ace1cfc1daa37dc
Alexandra Bugariu <alexandra.bugariu@inf.ethz.ch>
2017-06-14 12:50:35 +0000

    compiled without optimizations

--- elina_oct.mk
+++ elina_oct.mk
@@ -1,26 +1,26 @@
-number ?= 39
-start ?= 0
+number ?= 0
+start ?= 39
 
 all: compile test
 compile: 
 	cd elina_auxiliary ; \
-	clang -I /home/klee/klee_src/include -emit-llvm -c -g *.c ; \
+	clang -I /home/klee/klee_src/include -emit-llvm -O0 -c -g *.c ; \
 	cd ../elina_linearize ; \
-	clang -I /home/klee/klee_src/include -emit-llvm -c -g *.c ; \
+	clang -I /home/klee/klee_src/include -emit-llvm -O0 -c -g *.c ; \
 	cd ../partitions_api ; \
-	clang -I /home/klee/klee_src/include -emit-llvm -c -g *.c ; \
+	clang -I /home/klee/klee_src/include -emit-llvm -O0 -c -g *.c ; \
 	cd ../elina_oct ; \
-	clang -I /home/klee/klee_src/include -emit-llvm -c -g *.c ; \
+	clang -I /home/klee/klee_src/include -emit-llvm -O0 -c -g *.c ; \
 	cd ..       
  
 test:
 	cd elina_oct/tests; \
-        rm -rf klee-*; \
-	clang -I /home/klee/klee_src/include -emit-llvm -c -g *.c ; \
+        #rm -rf klee-*; \
+	clang -I /home/klee/klee_src/include -emit-llvm -O0 -c -g *.c ; \
         number=$(start) ; while [ $${number} -le $(number) ] ; do \
 		llvm-link ../../elina_auxiliary/*.bc ../../partitions_api/*.bc ../../elina_linearize/*.bc ../opt_oct_closure_comp_sparse.bc ../opt_oct_incr_closure_comp_sparse.bc ../opt_oct_closure_dense.bc ../opt_oct_incr_closure_dense.bc ../opt_oct_nary.bc ../opt_oct_resize.bc ../opt_oct_predicate.bc ../opt_oct_representation.bc ../opt_oct_transfer.bc ../opt_oct_hmat.bc ../opt_oct_closure_dense_scalar.bc ../opt_oct_incr_closure_dense_scalar.bc test_oct.bc test_oct$${number}.bc -o test$${number}.bc ; \
                 startTime=`date +%s` ; \
-                klee -allow-external-sym-calls -sym-malloc-bound=128 -max-depth=10 test$${number}.bc ; \
+                klee -allow-external-sym-calls -sym-malloc-bound=128 test$${number}.bc ; \
                 endTime=`date +%s` ; \
                 runtime=`expr $$endTime - $$startTime` ; \
                 echo "Execution time: $$runtime sec for test_oct$$number\n" ; \
