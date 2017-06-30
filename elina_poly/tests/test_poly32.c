464a858e0a0cf19ee3206838fcceab826db9d8bf
Alexandra Bugariu <alexandra.bugariu@inf.ethz.ch>
2017-06-30 13:09:37 +0000

    made the chain of polyhedra monotonically increasing before applying widening

--- elina_poly/tests/test_poly32.c
+++ elina_poly/tests/test_poly32.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv) {
 	int i = 0;
 	while (true) {
 		opt_pk_array_t * polyhedron2 = create_polyhedron(man, top, "2", dim);
-		wideningResult = opt_pk_widening(man, polyhedron1, polyhedron2);
+		wideningResult = opt_pk_widening(man, polyhedron1, opt_pk_join(man, false, polyhedron1, polyhedron2));
 		if (opt_pk_is_leq(man, wideningResult, polyhedron1)) {
 			break; // we reached a fixed point
 		}
