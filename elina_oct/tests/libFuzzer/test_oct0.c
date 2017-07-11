ebc0759f96ff565ac26fd70fc36211a1e4901f07
Alexandra Bugariu <alexandra_bugariu@yahoo.com>
2017-07-11 16:01:37 +0000

    fixed compilation errors

--- elina_oct/tests/libFuzzer/test_oct0.c
+++ elina_oct/tests/libFuzzer/test_oct0.c
@@ -14,10 +14,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t dataSize) {
 	opt_oct_t * top = opt_oct_top(man, dim, 0);
 	opt_oct_t * bottom = opt_oct_bottom(man, dim, 0);
 
-	opt_oct_t* octagon1 = create_octagon(man, top, "1", dim);
+	opt_oct_t* octagon1 = create_octagon(man, top, "1", dim, data, dataSize, dataIndex);
 
 	// bottom <= x
-	if(!opt_oct_is_leq(man, bottom, octagon1))){
+	if(!opt_oct_is_leq(man, bottom, octagon1)){
 		exit(ASSERTION_FAILED);
 	}
 	return 0;
