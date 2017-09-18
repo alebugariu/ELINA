d10ab1f8e852508b8beaf39d6fc31f5b63ee98dd
Alexandra Bugariu <alexandra_bugariu@yahoo.com>
2017-09-18 14:50:54 +0000

    Gagandeep's fix for elina_int (mpz) and addded the LD_LIBRARY_PATH in the makefile

--- elina_poly_libFuzzer.mk
+++ elina_poly_libFuzzer.mk
@@ -3,12 +3,12 @@ LIBS=-lmpfr -lgmp -lelinalinearize -lelinaux -loptpoly -lpartitions
 all: compile test
 compile: 
 	make CC=clang CFLAGS="-fsanitize-coverage=trace-pc-guard -fPIC -O0"
-	sudo make install    
+	sudo make install   
  
 test:
 	cd elina_poly/tests/libFuzzer; \
         number=$(start) ; while [ $${number} -le $(number) ] ; do \
-		clang -lstdc++ -fsanitize=address -fsanitize-coverage=trace-pc-guard -fPIC -O0 -DTHRESHOLD=0.75 -DNUM_DOUBLE \
+		LD_LIBRARY_PATH=/usr/local/lib/ clang -lstdc++ -fsanitize=address -fsanitize-coverage=trace-pc-guard -fPIC -O0 -DTHRESHOLD=0.75 -DNUM_DOUBLE \
                       -I /usr/local/include test_poly.c test_poly$${number}.c /home/libFuzzer.a -otest$${number} $(LIBS) ; \
                 startTime=`date +%s` ; \
 		./test$${number} -max_len=10000 -detect_leaks=0 -rss_limit_mb=8192 -timeout=3600 -print_final_stats=1 MY_CORPUS/ SEED_CORPUS/; \
