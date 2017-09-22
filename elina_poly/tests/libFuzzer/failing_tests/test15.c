31424c2589aae07744f0fada6412cf2c9fe59efd
Alexandra Bugariu <alexandra_bugariu@yahoo.com>
2017-09-22 15:24:03 +0000

    added 3 tests that fail because of MIN_INT/(-1)

--- elina_poly/tests/libFuzzer/failing_tests/test15.c
+++ elina_poly/tests/libFuzzer/failing_tests/test15.c
@@ -30,27 +30,33 @@ bool create_polyhedron(opt_pk_array_t** polyhedron, elina_manager_t* man,
 
 int main(int argc, char **argv) {
 	long dim = 2;
-	long nbcons = 2;
+	long nbcons = 3;
 
 	elina_manager_t * man = opt_pk_manager_alloc(false);
 	opt_pk_array_t * top = opt_pk_top(man, dim, 0);
+	opt_pk_array_t * bottom = opt_pk_bottom(man, dim, 0);
 
 	opt_pk_array_t* polyhedron1;
 
 	elina_lincons0_array_t lincons0 = elina_lincons0_array_make(nbcons);
-	lincons0.p[0].constyp = ELINA_CONS_EQ;
-	lincons0.p[1].constyp = ELINA_CONS_EQ;
-	long values1[3] = { 0, 131072, 1090519040};
-	elina_linexpr0_t * linexpr0 = create_linexpr0(dim, values1);
-	lincons0.p[0].linexpr0 = linexpr0;
-	long values2[3] = {-1012762419733073423, 864957149903712753, 15856113};
-	elina_linexpr0_t * linexpr1 = create_linexpr0(dim, values2);
-	lincons0.p[1].linexpr0 = linexpr1;
+		lincons0.p[0].constyp = ELINA_CONS_SUPEQ;
+		lincons0.p[1].constyp = ELINA_CONS_EQ;
+		lincons0.p[2].constyp = ELINA_CONS_EQ;
+		long values1[3] = {  -1099511627776, 0, 0};
+		elina_linexpr0_t * linexpr0 = create_linexpr0(dim, values1);
+		lincons0.p[0].linexpr0 = linexpr0;
+		long values2[3] = {-9223372036854775808, 0, 4261412864};
+		elina_linexpr0_t * linexpr1 = create_linexpr0(dim, values2);
+		lincons0.p[1].linexpr0 = linexpr1;
+		long values3[3] = {4737096, 9288674231451648, -72057413615747072};
+		elina_linexpr0_t * linexpr2 = create_linexpr0(dim, values3);
+		printf("linexpr2: %p",linexpr2);
+		fflush(stdout);
+		lincons0.p[2].linexpr0 = linexpr2;
 
 	if (create_polyhedron(&polyhedron1, man, top, dim, lincons0)) {
-		// x <= top
-		printf("polyhedron <= top: ");
-		printf("%d\n", opt_pk_is_leq(man, polyhedron1, top));
+		printf("polyhedron meet bottom == bottom: ");
+		printf("%d\n", opt_pk_is_eq(man, opt_pk_meet(man, false, polyhedron1, bottom), bottom));
 	}
 	return 0;
 }
