f9aeefb9ab62c8ae307f861c378b29071c46227d
Alexandra Bugariu <alexandra_bugariu@yahoo.com>
2017-09-28 19:33:13 +0000

    updated the makefile for octagons

--- elina_oct_libFuzzer.mk
+++ elina_oct_libFuzzer.mk
@@ -7,14 +7,14 @@ compile:
  
 test:
 	cd elina_oct/tests/libFuzzer; \
-    number=$(start) ; while [ $${number} -le $(number) ] ; do \
-		clang -lstdc++ -fsanitize=address -fsanitize-coverage=trace-pc-guard -fPIC -O0 -DTHRESHOLD=0.75 -DNUM_DOUBLE \
-				-I /usr/local/include test_oct.c test_oct$${number}.c /home/libFuzzer.a -o test$${number} $(LIBS); \
+    	number=$(start) ; while [ $${number} -le $(number) ] ; do \
+	clang -lstdc++  -fsanitize-coverage=trace-pc-guard -fPIC -O0 -DTHRESHOLD=0.75 -DNUM_DOUBLE \
+	-I /usr/local/include test_oct.c test_oct$${number}.c /home/libFuzzer.a -o test$${number} $(LIBS); \
         startTime=`date +%s` ; \
-		./test$${number} -max_len=10000 -detect_leaks=0 -rss_limit_mb=8192 -timeout=3600 -print_final_stats=1 MY_CORPUS/ SEED_CORPUS/; \
+	./test$${number} -max_len=10000 -detect_leaks=0 -rss_limit_mb=8192 -max_total_time=3600 -print_final_stats=1 MY_CORPUS/ SEED_CORPUS/; \
         endTime=`date +%s` ; \
         runtime=`expr $$endTime - $$startTime` ; \
         echo "Execution time: $$runtime sec for test_oct$$number\n" ; \
-		number=`expr $$number + 1` ; \
+	number=`expr $$number + 1` ; \
     	done; \
         true
